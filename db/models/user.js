52ef33c453da153073dce07dd45a8ba61d019a83
adamvader <adamonglt@hotmail.com>
2022-10-06 18:33:26 +0800

    created seed data for users table

--- db/models/user.js
+++ db/models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -13,14 +11,17 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  user.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    phone_number: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
+  user.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      phone_number: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
   return user;
 };
---
+++ db/seeders/20221006100607-seed-users.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Adam",
+          email: "adam@mail.com",
+          password: "password",
+          phone_number: "6598182227",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Amy",
+          email: "amy@mail.com",
+          password: "password",
+          phone_number: "81364260780",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Wei Yu",
+          email: "wei_yu@mail.com",
+          password: "password",
+          phone_number: "62778470100",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
