c260bee76ce6b23051658f7ff5204a73ca751e08
adamvader <adamonglt@hotmail.com>
2022-10-06 19:30:09 +0800

    created seed data for reservations table

---
+++ db/seeders/20221006110619-seed-reservations.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "reservations",
+      [
+        {
+          customer_id: 1,
+          properties_id: 1,
+          start_date: new Date(2022, 11, 1),
+          end_date: new Date(2022, 11, 3),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          customer_id: 3,
+          properties_id: 1,
+          start_date: new Date(2022, 11, 4),
+          end_date: new Date(2022, 11, 9),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          customer_id: 3,
+          properties_id: 2,
+          start_date: new Date(2022, 12, 25),
+          end_date: new Date(2023, 1, 1),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("reservations", null, {});
+  },
+};
