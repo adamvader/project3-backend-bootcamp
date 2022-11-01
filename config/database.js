c0061edf27b75ba4bd2ac48b7356a34d69a98bf1
weiyu95 <weiyu95@gmail.com>
2022-11-01 20:02:03 +0800

    first commit to heroku

--- config/database.js
+++ config/database.js
@@ -8,4 +8,15 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
